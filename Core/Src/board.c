2b5341c36a2186c69a86384be204a8e6346dd40f
cempod <cempod@gmail.com>
2024-08-06 15:49:46 +0300

    NVIC priority

--- Core/Src/board.c
+++ Core/Src/board.c
@@ -8,6 +8,9 @@ board_init(void) {
 
     rcc_init();
     
+    NVIC_SetPriorityGrouping(3U);
+    NVIC_SetPriority(SysTick_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 15, 0));
+
     gpio_init();
 
     spi_config();
